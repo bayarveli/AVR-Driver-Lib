fc95f1af35c6f483d9a0aa91805dc2d585ccd141
bayarveli <bayar.veli@gmail.com>
2020-04-21 12:32:45 +0300

    Elf to Hex Conversion is added.

--- BuildFunctions.py
+++ BuildFunctions.py
@@ -4,20 +4,30 @@ Import('env')
 
 def createExecutable(env, executableName, outputDirectory, sourceFiles):
    
+    print (executableName)
     objectList = []
     
     for sourceFile in sourceFiles:
         sourceFilePath = File(sourceFile).get_abspath()       
         sourceFileName = File(sourceFile).name
-    
+           
         objectFilePath = outputDirectory + '/' + os.path.splitext(sourceFileName)[0]       
         objectFile = env.Object(outputDirectory + '/' + objectFilePath + '.o', sourceFilePath)
-        
+         
         objectList.append(objectFile)
-    
-    program = env.Program(outputDirectory + '/' + executableName + '.elf', objectList)   
+      
+    program = env.Program(outputDirectory + '/' + executableName + '.elf', objectList)
+
     env.Alias(executableName, program)
-     
+   
+    return program
+
+def createHexFile(env, hexName, outputDirectory, elfFile):
+    
+    program = env.Command(outputDirectory + '/' + hexName + '.hex', outputDirectory + '/' + elfFile + '.elf', env['OBJCOPY'] + ' -O ihex -R .eeprom $SOURCE $TARGET')
+    
+    env.Alias(hexName + 'Hex', program)
+    
     return program
 
 def createLibrary(env, libraryName, outputDirectory, sourceFiles):
@@ -41,3 +51,4 @@ def createLibrary(env, libraryName, outputDirectory, sourceFiles):
          
 env.AddMethod(createExecutable, 'createExecutable')
 env.AddMethod(createLibrary, 'createLibrary')
+env.AddMethod(createHexFile, 'createHexFile')
--- EnvironmentSettings.py
+++ EnvironmentSettings.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import scons.SCons.Tool.cc as cc
+import scons.SCons.Builder as builder
 
 env = Environment()
 
@@ -8,28 +9,29 @@ def _echospawn(sh, escape, cmd, args, env):
     return subprocess.call(args)
 
 # / ile yazılan flagleri - ye dönüştürüyor.
-cc.generate(env)
+# cc.generate(env)
 
 # avr-gcc is not recognized hatasını kaldırıyor.
 env['SPAWN']  = _echospawn
 env['ESCAPE'] = lambda x:x
 env['CC'] = 'avr-gcc'
+env['OBJCOPY'] = 'avr-objcopy'
 env['AR'] = 'avr-ar'
 env['CCFLAGS'] = []
 env['LINKFLAGS'] = []
 env['ARFLAGS'] = []
-env['OBJSUFFIX'] = '.o'
-env['PROGSUFFIX'] = '.elf'
-env['LIBSUFFIX'] = ''
-env['LIBDIRPREFIX'] = '-L'
-env['LIBLINKPREFIX'] = '-l'
+# env['OBJSUFFIX'] = '.o'
+# env['PROGSUFFIX'] = '.elf'
+# env['LIBSUFFIX'] = ''
+# env['LIBDIRPREFIX'] = '-L'
+# env['LIBLINKPREFIX'] = '-l'
 
 #env['_LIBDIRFLAGS'] = '${_concat(LIBDIRPREFIX, LIBPATH, LIBDIRSUFFIX, __env__)}'
 #env['_CPPINCFLAGS'] = '${_concat(INCPREFIX, CPPPATH, INCSUFFIX, __env__)}'
 
-env['LINKCOM'] = "$CC $LINKFLAGS $_CCCOMCOM -o $TARGET $SOURCE $_LIBDIRFLAGS $_LIBFLAGS"
-env['ARCOM'] = "$AR $ARFLAGS $TARGET $SOURCES"
-env['CCCOM'] = "$CC -o $TARGET -c $CFLAGS $CCFLAGS $_CCCOMCOM $SOURCES"
+# env['LINKCOM'] = "$CC $LINKFLAGS $_CCCOMCOM -o $TARGET $SOURCE $_LIBDIRFLAGS $_LIBFLAGS"
+# env['ARCOM'] = "$AR $ARFLAGS $TARGET $SOURCES"
+# env['CCCOM'] = "$CC -o $TARGET -c $CFLAGS $CCFLAGS $_CCCOMCOM $SOURCES"
 
 env.Append(CCFLAGS = '-mmcu=atmega32u4')
 env.Append(CCFLAGS = '-g')
@@ -40,7 +42,6 @@ env.Append(LINKFLAGS = '-mmcu=atmega32u4')
 
 env.Append(ARFLAGS = 'rcs')
 
-
-#print (env.Dump())
+print (env.Dump())
 
 Export('env')
